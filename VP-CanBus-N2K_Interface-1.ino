208c136393bdfe1aa394914ab8d9b2975b31ce78
st599 <st599@zepler.net>
2026-05-21 21:13:28 +0100

    Added MQTT Client

--- VP-CanBus-N2K_Interface-1.ino
+++ VP-CanBus-N2K_Interface-1.ino
@@ -1,3 +1,20 @@
+//
+//   Volvo Penta CANbus to SignalK via MQTT Gateway
+//
+//   Filename        :   vp_canbus_to_n2k.py
+//   Description     :   Sends boat engine data to MQTT Gateway
+//   Date            :   22/05/2026
+//   Author          :   Simon Thompson
+//   Copyright       :   Simon Thompson 2026
+//                   :   Buhhe 2021
+//   Dependencies    :   Arduino IDE, ESP32 board support, MCP_CAN library, SPI library, WiFi library, PubSubClient library
+//   License         :   GNU Lesser General Public License v2.1 or later
+//   Repository      :   https://github.com/st599/VolvoPenta-MQTT_Interface 
+//   Based on        :   This code is based on the work of Buhhe and has been modified to 
+//                       read specific J1939 data from the Volvo Penta CANbus and send it to a SignalK server. 
+//                       The original code can be found at https://github.com/buhhe/VolvoPenta-N2K_Interface.
+
+
 /*
   This code is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -12,21 +29,6 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-//
-//   Volvo Penta CANbus to SignalK via MQTT Gateway
-//
-//   Filename        :   vp_canbus_to_n2k.py
-//   Description     :   Sends boat engine data to MQTT Gateway
-//   Date            :   22/05/2026
-//   Author          :   Simon Thompson
-//   Copyright       :   Simon Thompson 2026
-//                   :   Buhhe 2021
-//   Dependencies    :   Arduino IDE, ESP32 board support, MCP_CAN library, SPI library
-//   License         :   GNU Lesser General Public License v2.1 or later
-//   Repository      :   https://github.com/st599/VolvoPenta-MQTT_Interface 
-//   Based on        :   This code is based on the work of Buhhe and has been modified to 
-//                       read specific J1939 data from the Volvo Penta CANbus and send it to a SignalK server. 
-//                       The original code can be found at https://github.com/buhhe/VolvoPenta-N2K_Interface.
 
 //
 //   SYSTEM IMPORTS
@@ -35,7 +37,8 @@
 #include <Preferences.h>
 #include <mcp_can.h>
 #include <SPI.h>
-
+#include <WiFi.h>
+#include <PubSubClient.h>
 
 //
 //  AUTHOR IMPORTS
@@ -44,30 +47,58 @@
 //
 //  GLOBAL CONSTANTS
 //
+const char *ssid = "xxxxx"; // Enter your WiFi name
+const char *password = "xxxxx";  // Enter WiFi password
+const char *mqtt_broker = "192.168.0.1";  // MQTT broker address (e.g., IP address or hostname of the SignalK server)
+const int mqtt_port = 1883;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
 #define CAN0_INT 17                            
 MCP_CAN CAN0(5); 
 
 const char Description[] = "Description: Volvo Penta->N2K interface. Read J1939 data from VP canbus, \nconvert it and send it to a SignalK server.\n\n";
 const char mqttPrefix[] = "W/signalk/";  // MQTT topic prefix for SignalK data
+const char mmsi[] = "123456789";  // MMSI number for the vessel, used in MQTT topics
 const char runTimeTopic[] = "/vessels/self/propulsion/main/runTime";
 const char coolantTempTopic[] = "/vessels/self/propulsion/main/coolantTemperature";
 const char alternatorVoltageTopic[] = "/vessels/self/propulsion/main/alternatorVoltage";
 const char rpmTopic[] = "/vessels/self/propulsion/main/revolutions";
 
 
-
-
 //
 //   FUNCTION DEFINITIONS
 //
-void setup() {
-  uint32_t id = 0, i;
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  // Initialize serial communication, WiFi connection, and CAN bus
   Serial.begin(115200);
   delay(50);
 
-  SayHello();  // print some useful information to USB-serial
+  // Connect to WiFi
+  setup_wifi();
 
+  //  Initialize CAN bus
   if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK) // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
     Serial.println("MCP2515 Initialized Successfully");
   else
@@ -79,9 +110,9 @@ void setup() {
   CAN0.setMode(MCP_LISTENONLY);   // Set operation mode to listen only. We don't want to write anything to the VP CAN-Bus
   pinMode(CAN0_INT, INPUT);       // Configuring pin for INT input
 
+  // Initialize MQTT client
+  client.setServer(mqtt_broker, mqtt_port);
 
-
-  preferences.begin("nvs", false);                          // Open nonvolatile storage (nvs)
   delay(200);
 }
 
@@ -129,6 +160,20 @@ void loop()
     if ( SendSTAT == 0x07 )   // are the three values available
     {
       Serial.printf("Battery:%.2f Hours: %.2f Temperature: %.2f Revolutions (Hz): %.2f\n", AlternatorVoltage, EngineHours, CoolantTemperature, Revolutions_Hz);
+      char str[256];
+      strcpy(str, mqttPrefix);
+      strcat(str, mmsi);  
+      strcat(str, runTimeTopic);
+      client.publish(str, String(EngineHours).c_str(), true);  // Publish engine hours to MQTT topic
+      strcpy(str, mqttPrefix);
+      strcat(str, mmsi);
+      strcat(str, coolantTemperatureTopic);
+      client.publish(str, String(CoolantTemperature).c_str(), true);  // Publish coolant temperature to MQTT topic
+      strcpy(str, mqttPrefix);
+      strcat(str, mmsi);
+      strcat(str, revolutionsTopic);
+      client.publish(str, String(Revolutions_Hz).c_str(), true);  // Publish revolutions to MQTT topic
+      strcpy(str, ""); 
       CoolantTemperature = AlternatorVoltage = EngineHours = Revolutions_Hz = 0.0; 
       SendSTAT = 0;
     }
